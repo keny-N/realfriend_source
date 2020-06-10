0e341a7359b7e210841fba8ccfb6f0a26d25c320
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-06-10 12:41:27 +0900

    変更

--- mysql/db/init.sql
+++ mysql/db/init.sql
@@ -1,10 +1,90 @@
-CREATE DATABASE sample_db;
-use sample_db;
+CREATE DATABASE realfriend;
+use realfriend;
 
-CREATE TABLE users (
-  id int(10) unsigned not null auto_increment,
-  name varchar(255) not null,
-  created_time datetime not null default current_timestamp,
-  updated_time datetime not null default current_timestamp on update current_timestamp,
-  primary key (id)
-);
+--
+-- Database: `realfriend`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `friend`
+--
+
+CREATE TABLE `friend` (
+  `friend_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `friend_name` varchar(100) NOT NULL,
+  `friend_img` blob,
+  `friend_favorable` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `friend`
+--
+
+INSERT INTO `friend` (`friend_id`, `user_id`, `friend_name`, `friend_img`, `friend_favorable`) VALUES
+(1, 1, 'friend_name', NULL, 0),
+(2, 1, 'friendkun', NULL, 2);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_pass` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_name`, `user_pass`) VALUES
+(1, 'test君', 'password'),
+(2, 'testmen', 'password');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `friend`
+--
+ALTER TABLE `friend`
+  ADD PRIMARY KEY (`friend_id`),
+  ADD KEY `realfriend.friend_user_user_id_fk` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `friend`
+--
+ALTER TABLE `friend`
+  MODIFY `friend_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `friend`
+--
+ALTER TABLE `friend`
+  ADD CONSTRAINT `realfriend.friend_user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
