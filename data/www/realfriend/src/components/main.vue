65fc6bc6eefc70eaacab7ee52ca84532215715b8
ASO1701003 <1701003@st.asojuku.ac.jp>
2020-06-15 12:04:08 +0900

    コンポーネントつくった
    ヘッターとメイン

---
+++ data/www/realfriend/src/components/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+        <hdr></hdr>
+        <img src="../assets/realogo.png">
+        <h1>メイン画面</h1>
+    </div>
+</template>
+
+<script>
+    import hdr from '@/components/hdr'
+    export default {
+        name: "Main",
+        components: { hdr }
+    };
+</script>
+
+<style scoped>
+
+</style>
