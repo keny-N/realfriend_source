9e583972014bf8e8d7b38f4049f66c77b8e902c1
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-06-19 03:52:18 +0900

    bootstrap install

--- data/www/realfriend/src/components/GameHeader.vue
+++ data/www/realfriend/src/components/GameHeader.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="gameheader">
-        <router-link to="/"><button>メイン</button></router-link>
-        <router-link to="/game"><button>ゲーム</button></router-link>
-    </div>
+  <div class="gameheader">
+    <router-link to="/">
+      <button type="button" class="btn btn-primary btn-sm">メイン</button>
+    </router-link>
+    <router-link to="/game">
+      <button type="button" class="btn btn-primary btn-sm">ゲーム</button>
+    </router-link>
+  </div>
 </template>
 
 <script>
--- data/www/realfriend/src/components/main.vue
+++ data/www/realfriend/src/components/main.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="main">
-        <GameHeader></GameHeader>
-        <img src="../assets/realogo.png">
-        <h1>メイン画面</h1>
-    </div>
+  <div class="main">
+    <GameHeader></GameHeader>
+    <img src="../assets/realogo.png">
+    <h1>メイン画面</h1>
+    <News></News>
+  </div>
 </template>
 
 <script>
     import GameHeader from '@/components/GameHeader'
+    import News from "@/components/News";
+
     export default {
         name: "Main",
-        components: { GameHeader },
+        components: {GameHeader, News},
     };
 </script>
 
