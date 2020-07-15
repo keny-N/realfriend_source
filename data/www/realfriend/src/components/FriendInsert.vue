881e786cec97bf4dda6bcf098b0d1da77134b361
ASO1701003 <1701003@st.asojuku.ac.jp>
2020-07-15 20:25:57 +0900

    フレンド一覧、登録

---
+++ data/www/realfriend/src/components/FriendInsert.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="friendinsert">
+    <button v-on:click="openModal">フレンド登録</button>
+    <div class="overlay" v-show="showContent">
+      <div class="content">
+        <h4>フレンド登録</h4>
+        <div class="card-body">
+          <div>
+            <img v-on:src="imageData" v-if="imageData">
+          </div>
+          <h4 class="card-title">画像を選んでください。</h4>
+          <input type="file" accept="image/*" @change="onImgRegister($event)" v-model="friendImg">
+        </div>
+        <div>フレンド名前：<input v-model="friendName"></div>
+        <button class="float-left" v-on:click="registerFrined">登録</button>
+        <button class="float-right" v-on:click="closeModal">取り消し</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "FriendInsert",
+    data() {
+      return {
+        showContent: false, //モーダルを非表示している
+        imageData: '',//画像
+        friendName: '',//フレンドの名前
+        postUrl: 'https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend/friends/one'//フレンド登録URL
+      }
+    },
+    methods: {
+      openModal: function () {
+        this.showContent = true
+      },
+      closeModal: function () {
+        this.showContent = false
+      },
+      onImgRegister(e) {
+        const files = e.target.files
+
+        if (files.length > 0) {
+
+          const file = files[0]
+          const reader = new FileReader()
+          reader.onload = (e) => {
+            this.imageData = e.target.result
+          }
+          reader.readAsDataURL(file)
+        }
+      },
+      registerFrined() {
+        if (this.friendName.length > 20 || this.friendName.length < 1) {
+          //２０文字以内かつ一文字以上でない時に実行される。
+        }
+
+        let name = this.friendName
+        //faceApiに顔データを送信
+        console.log('put送信します')
+        this.axios.post(this.postUrl, {
+          user_id: 1,
+          friend_name: name,
+          //現在はパスが確定していないためパスっぽい文字を入れているだけです。
+          friend_img: '/jk/matuo'
+        }).then(function (response) {
+          if (response.data.isSuccess) {
+            console.log(response)
+            this.showContent = false
+          }
+        }).catch(function (error) {
+          console.log(error)
+          this.showContent = true
+        })
+        //メイン画面を更新する処理
+        this.$router.go({path: this.$router.currentRoute.path, force: true})
+        console.log('以下')
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .overlay {
+    /*　要素を重ねた時の順番　*/
+    z-index: 1;
+
+    /*　画面全体を覆う設定　*/
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    /*　画面の中央に要素を表示させる設定　*/
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+
+  .content {
+    z-index: 2;
+    width: 50%;
+    padding: 1em;
+    background: #fff;
+  }
+</style>
+
--- data/www/realfriend/src/components/Main.vue
+++ data/www/realfriend/src/components/Main.vue
@@ -2,34 +2,37 @@
   <div class="main" :style="{ 'background-image': 'url(' + backgroundImageSrc + ')' }">
     <GameHeader></GameHeader>
     <FriendList class="listposition"></FriendList>
+    <FriendInsert></FriendInsert>
     <News></News>
   </div>
 </template>
 
 <script>
   import GameHeader from '@/components/GameHeader'
-  import News from "@/components/News";
-  import FriendList from "@/components/FriendList";
+  import News from "@/components/News"
+  import FriendList from "@/components/FriendList"
+  import FriendInsert from "@/components/FriendInsert";
 
   export default {
     name: "Main",
-    components: {GameHeader, News,FriendList},
-    data () {
+    components: {GameHeader, News, FriendList,FriendInsert},
+    data() {
       return {
         backgroundImageSrc: require("@/assets/main_back.jpg")
       }
     }
-  };
+  }
 </script>
 
 <style scoped>
-  .main{
+  .main {
     background: no-repeat center;
     background-size: cover;
     /*mainのサイズをちょうど画面と同じにする。*/
     height: 100vh;
   }
-  .listposition{
+
+  .listposition {
     margin: 50vh auto 0;
     transform: translateY(-50%);
   }
