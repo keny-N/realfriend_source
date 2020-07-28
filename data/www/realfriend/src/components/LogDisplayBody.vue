d0203f8db3077a04e510733b9cd96728ae1db78f
ganngiNote <ganngi77@gmail.com>
2020-07-28 09:59:59 +0900

    とりあえずエラー処理ができるようになった

--- data/www/realfriend/src/components/LogDisplayBody.vue
+++ data/www/realfriend/src/components/LogDisplayBody.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="news-body">
-    <InformationList :title="UpdateInformationTitle" :body-data="UpdateInformationBody"></InformationList>
-<!--    <InformationList :title="title" :body-data="LogInformation"></InformationList>-->
+    <InformationList :data-list="updateInformation"></InformationList>
+    <InformationList :data-list="logInformation"></InformationList>
   </div>
 </template>
 
@@ -13,18 +13,62 @@
     components: {InformationList},
     data() {
       return {
-        UpdateInformationTitle: "アップデート情報",
-        UpdateInformationBody: [{title:"全米が驚愕",body:"あほくさ"}],
-        LogInformation: {
+        updateInformation: {
+          title: "更新情報", body: [{title: "全米が驚愕", body: "あほくさ"}]
+        },
+        logInformation: {
           title: "ログ情報", body: [{title: "07/07/02/21", body: "削除完了"}]
         },
+        USER_ID: "aaa",
+        BASE_URL: "https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend",
+        LOG_URL: this.BASE_URL + "/user/" + this.USER_ID + "/log",
+        apiError:false
       }
     },
 
-    methods: {},
+    methods: {
+      // updateGet: async function() {
+      //   const URL = ""
+      //   await $.axios.get(URL)
+      //     .then(response => (this.UpdateInformation = response))
+      //     .catch(error => alert('エラーが発生したためリロードしてくださいｋ'))
+      //
+      // },
+      logGet: async function () {
+        await this.axios.get(this.LOG_URL)
+          .then(response => response)
+          .catch(error =>{
+            this.apiError = true
+            console.log(error.response.status)
+          })
+        console.log(11111111111111)
+      },
+      dataGet: async function () {
+        // await Promis.all([this.updateGet,this.logGet])
+        // await Promis.all([this.logGet()])
+         await this.logGet()
+      }
+    },
+
+    async mounted() {
+      // this.axios.get(this.LOG_URL).then(response=>console.log(response)).catch(error=>console.log(error.response.status))
+      // console.log(1111111111111111111)
+      await this.dataGet()
+      if (this.apiError === true){
+        alert("エラーが発生しました。再度実行してください。")
+      }
+      console.log(this.apiError)
+      // console.log(this.apiError,222222222222)
+      // console.log(this.logInformation)
+
+    }
   }
 </script>
 
 <style scoped>
-
+  .news-body {
+    width: 70vh;
+    height: 100%;
+    margin: 0 auto;
+  }
 </style>
--- data/www/realfriend/src/components/LogDisplayList.vue
+++ data/www/realfriend/src/components/LogDisplayList.vue
@@ -1,23 +1,22 @@
 <template>
-  <h1>{{title}}</h1>
-  <h2>{{bodyData}}</h2>
-
-  <ol>
-    <li v-for="data in bodyData" :key="data">{{data.title}}:{{data.body}}</li>
-  </ol>
+  <div>
+    <h1>{{dataList.title}}</h1>
+    <div class="news-list overflow-auto">
+      <ol>
+        <li v-for="data in dataList.body" :key="data.title">{{data.title}}:{{data.body}}</li>
+      </ol>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     name: "LogDisplay",
     props: {
-      title: {
-        type: String,
-      },
-      bodyData:{
-        type:Object,
-      },
-    },
+      dataList: {
+        type: Object
+      }
+    }
   }
 
 </script>
@@ -26,5 +25,16 @@
   li {
     list-style: none;
     margin-left: -20px;
+  }
+  div:first-child{
+    height: 30vh;
+    margin-bottom: 5vh;
+  }
+  h1{
+    height: 5vh;
+  }
+  .news-list{
+    border: #1b1e21 solid 1px;
+    height: 25vh;
   }
 </style>
