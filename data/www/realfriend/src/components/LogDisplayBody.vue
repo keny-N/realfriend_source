bb85118b4b3bc90efabf549bcfc4cfeac77e335c
ganngiNote <ganngi77@gmail.com>
2020-07-28 12:14:25 +0900

    ログ完成

--- data/www/realfriend/src/components/LogDisplayBody.vue
+++ data/www/realfriend/src/components/LogDisplayBody.vue
@@ -30,52 +30,30 @@
         await this.axios.get(url)
           .then(response =>
             response.data.logs.forEach(tmpData=> {
-              // console.log(tmpData)
               data.body.push({title:tmpData[1], body:tmpData[0]})
             })
           )
           .catch(error => {
             this.apiError = true
-            console.log(error.response.status)
+            // console.log(error.response.status)
           })
-        // console.log(data)
       },
-      // updateGet: async function() {
-      //   const URL = ""
-      //   await $.axios.get(URL)
-      //     .then(response => (this.UpdateInformation = response))
-      //     .catch(error => alert('エラーが発生したためリロードしてくださいｋ'))
-      //
-      // },
-      // logGet: async function () {
-      //   await this.axios.get(this.LOG_URL)
-      //     .then(response => response)
-      //     .catch(error =>{
-      //       this.apiError = true
-      //       console.log(error.response.status)
-      //     })
+
       dataGet: async function () {
         const LOG_URL = this.BASE_URL + "/users/" + this.USER_ID + "/log"
         // await Promis.all([this.updateGet,this.logGet])
         // await Promis.all([this.logGet()])
         await this.apiGet(LOG_URL, this.logInformation)
-
-
       },
     },
 
     async mounted() {
-      // this.axios.get(this.LOG_URL).then(response=>console.log(response)).catch(error=>console.log(error.response.status))
-      // console.log(1111111111111111111)
       await this.dataGet()
       if (this.apiError === true) {
         alert("エラーが発生しました。再度実行してください。")
       }
-      console.log(this.apiError)
-      console.log(this.logInformation)
-      // console.log(this.apiError,222222222222)
+      // console.log(this.apiError)
       // console.log(this.logInformation)
-
     }
   }
 </script>
