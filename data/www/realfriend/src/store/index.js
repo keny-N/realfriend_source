96390b58f9bd58ab5984f0164ecffaf75d18b68f
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-08-02 20:00:27 +0900

    vuex module 導入テスト

--- data/www/realfriend/src/store/index.js
+++ data/www/realfriend/src/store/index.js
@@ -1,46 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import counter from '@/store/modules/Counter'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-    aCount: 0,
-    bCount: 0,
-    cCount: 0,
-    dCount: 0
+  modules: {
+    counter,
   },
-  mutations: {
-    increment(state, payload) {
-      if (payload == "a") {
-        state.aCount++
-      } else if (payload == "b") {
-        state.bCount++
-      } else if (payload == "c") {
-        state.cCount++
-      } else if (payload == "d") {
-        state.dCount++
-      }
-      state.aCount++
-    },
-    resetCounter(state) {
-      state.aCount = 0
-      state.bCount = 0
-      state.cCount = 0
-      state.dCount = 0
-    }
-  },
-  actions: {
-    increment: ({commit}, payload) => {
-      setTimeout(() => {
-        commit('increment', payload)
-      }, 1000)
-    },
-    resetCounter: ({commit}) => {
-      setTimeout(() => {
-        commit('resetCounter')
-      }, 100)
-    }
-  },
-  modules: {}
 })
---
+++ data/www/realfriend/src/store/modules/template.js
@@ -0,0 +1,6 @@
+export default {
+  namespaced: true,  //モジュールを名前空間に分ける
+  state: {},
+  mutations: {},
+  actions: {}
+}
