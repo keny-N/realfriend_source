b85602af06662dca7684fc7883bdd3032ed92795
Nakamura Kenya <pinpondream@yahoo.co.jp>
2020-08-06 15:06:05 +0900

    同期処理

--- data/www/realfriend/src/components/Camera.vue
+++ data/www/realfriend/src/components/Camera.vue
@@ -6,7 +6,7 @@
       <video ref="video" id="video" width="500" height="500" autoplay muted class="camera-size"></video>
       <canvas ref="canvas" id="canvas" width="500" height="500"></canvas>
       <div>
-        <button v-on:click="recStart">Start</button>
+        <button v-bind:disabled="isPush" v-on:click="recStart">Start</button>
         <button ref="stop" v-on:click="recStop">Stop</button>
       </div>
     </div>
@@ -35,7 +35,8 @@
                 audioData: [],
                 bufferSize: 1024,
                 wav: null,
-                postUrl: 'https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend/emotionjudgement'
+                postUrl: 'https://abwp9ub4n8.execute-api.ap-northeast-1.amazonaws.com/realfriend/emotionjudgement',
+                isPush: false
             }
         },
         mounted() {
@@ -61,13 +62,13 @@
                 }, {
                     headers: {
                         //トークンをヘッダーに付与する
-                        'Authorization': 'CQ8yKBb2LN4k_hGvDXIPPlW5lEa4z1CtRRX0tFOZEFVi1Nn5OB_9tmfr39WqZahjt7IhDXXbbc12PXuzIXlbQA'
+                        'Authorization': '9oLgyqAa-vbXbs6zRt4AwP1Q1AOOvUsJSlUaXxZBOf5OYWEYGmiNaUrIjCH-jBSjAknZ4yLQnbjLqjaHkNtH9A'
                     }
                 }).then(function (response) {
                     console.log(response.status)
                     if (response.status == 200) {
                         console.log(response)
-                        me.msg = response.data.result
+                        me.$emit('updateMsg', response.data.result)
                     } else {
                         // me.msg = response.data.result
                         console.log(response)
@@ -77,20 +78,16 @@
                     me.msg = "catch_error"
                 })
             },
-            // empath(wav) {
-            //     console.log("empath起動")
-            //     console.log(wav)
-            //     this.axios.post('https://23gjh3gnd7.execute-api.ap-northeast-1.amazonaws.com/empath/mastuo_lamda_test', {
-            //         wav: String(wav)
-            //     }).then(function (response) {
-            //         console.log(response)
-            //     }).catch(function (error) {
-            //         console.log(error)
-            //     })
-            //
-            // },
             capture() {
                 const EXECUTE = async () => {
+                    const onAudioProcess = () => {
+                        return new Promise((((resolve, reject) => {
+                            this.scriptProcessor.onaudioprocess = this.onAudioProcess
+                            console.log("audioprocess開始")
+                            resolve()
+                        })))
+                    }
+
                     const createImage = () => {
                         return new Promise(((resolve, reject) => {
                             //カメラが写っている範囲を指定し、その領域を画像として切り取る
@@ -100,33 +97,131 @@
                             //画像データをbase64にエンコード
                             this.image = this.canvas.toDataURL("image/jpeg")
                             this.image = this.image.substr(23)
+                            console.log("画像生成開始")
                             resolve()
                         }))
                     }
 
-                    const createWav = () => {
-                        return new Promise(((resolve, reject) => {
-                            this.exportWAV()
-                            resolve()
-                        }))
+
+                    const encodeWav = () => {
+                        return new Promise((((resolve, reject) => {
+
+                            console.log("音声開始")
+                            let me = this
+
+                            let encodeWAV = function (samples, sampleRate) {
+                                let buffer = new ArrayBuffer(44 + samples.length * 2)
+                                let view = new DataView(buffer)
+
+                                let writeString = function (view, offset, string) {
+                                    for (let i = 0; i < string.length; i++) {
+                                        view.setUint8(offset + i, string.charCodeAt(i))
+                                    }
+                                }
+
+                                let floatTo16BitPCM = function (output, offset, input) {
+                                    for (let i = 0; i < input.length; i++, offset += 2) {
+                                        let s = Math.max(-1, Math.min(1, input[i]))
+                                        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true)
+                                    }
+                                }
+
+                                writeString(view, 0, 'RIFF') // RIFFヘッダ
+                                view.setUint32(4, 32 + samples.length * 2, true) // これ以降のファイルサイズ
+                                writeString(view, 8, 'WAVE') // WAVEヘッダ
+                                writeString(view, 12, 'fmt ') // fmtチャンク
+                                view.setUint32(16, 16, true) // fmtチャンクのバイト数
+                                view.setUint16(20, 1, true) // フォーマットID
+                                view.setUint16(22, 1, true) // チャンネル数
+                                view.setUint32(24, sampleRate, true) // サンプリングレート
+                                view.setUint32(28, sampleRate * 2, true) // データ速度
+                                view.setUint16(32, 2, true) // ブロックサイズ
+                                view.setUint16(34, 16, true) // サンプルあたりのビット数
+                                writeString(view, 36, 'data')// dataチャンク
+                                view.setUint32(40, samples.length * 2, true) // 波形データのバイト数
+                                floatTo16BitPCM(view, 44, samples) // 波形データ
+
+
+                                return view
+                            }
+
+                            let mergeBuffers = function (audioData) {
+                                let sampleLength = 0
+                                for (let i = 0; i < audioData.length; i++) {
+                                    sampleLength += audioData[i].length
+                                }
+                                let samples = new Float32Array(sampleLength)
+                                let sampleIdx = 0
+                                for (let i = 0; i < audioData.length; i++) {
+                                    for (let j = 0; j < audioData[i].length; j++) {
+                                        samples[sampleIdx] = audioData[i][j]
+                                        sampleIdx++
+                                    }
+                                }
+
+                                return samples
+                            }
+
+                            let dataview = encodeWAV(mergeBuffers(this.audioData), this.audio_sample_rate)
+                            let audioBlob = new Blob([dataview], {type: 'audio/wav'})
+                            console.log(dataview)
+                            console.log(audioBlob)
+                            let reader = new FileReader()
+                            reader.readAsDataURL(audioBlob)
+
+                            reader.onload = function () {
+                                me.wav = reader.result.substr(22)
+                                console.log("fileOnload完了")
+                                resolve()
+                            }
+
+                            this.audioData = []
+                            // let myURL = window.URL || window.webkitURL
+                            // let url = myURL.createObjectURL(audioBlob)
+                            // return url
+
+                        })))
                     }
 
                     const runApi = () => {
                         return new Promise(((resolve, reject) => {
                             //感情測定APIを実行[empath|faceapi]
                             this.emotionJudgement()
+                            console.log("APIに送信開始")
                             resolve()
                         }))
                     }
 
+                    const scriptProcessor = () => {
+                        return new Promise((((resolve, reject) => {
+                            this.scriptProcessor.connect(this.audioContext.destination)
+                            console.log("scriptProcessor開始")
+                            resolve()
+                        })))
+                    }
+
+                    const updateMsg = () => {
+                        return new Promise((((resolve, reject) => {
+                            //親コンポーネント(GameBodyのupdateMSGを実行する)
+                            this.$emit('updateMsg', this.msg)
+                            console.log("メッセージこうしん開始")
+                            resolve()
+                        })))
+                    }
+
+                    await onAudioProcess()
+                    console.log("audioprocess完了")
                     await createImage()
                     console.log("画像生成完了")
-                    await createWav()
+                    await encodeWav()
                     console.log("音声生成完了")
+                    // await createWav()
+                    // console.log("音声生成完了")
+                    await scriptProcessor()
+                    console.log("scriptProcessor完了")
                     await runApi()
                     console.log("APIに送信完了")
-                    //親コンポーネント(GameBodyのupdateMSGを実行する)
-                    this.$emit('updateMsg', this.msg)
+                    await updateMsg()
                     console.log("メッセージの更新完了")
                 }
                 EXECUTE()
@@ -139,78 +234,6 @@
             //         // this.stopButton.setAttribute('disabled', 'disabled')
             //     })
             // },
-            exportWAV() {
-                let me = this
-
-                let encodeWAV = function (samples, sampleRate) {
-                    let buffer = new ArrayBuffer(44 + samples.length * 2)
-                    let view = new DataView(buffer)
-
-                    let writeString = function (view, offset, string) {
-                        for (let i = 0; i < string.length; i++) {
-                            view.setUint8(offset + i, string.charCodeAt(i))
-                        }
-                    }
-
-                    let floatTo16BitPCM = function (output, offset, input) {
-                        for (let i = 0; i < input.length; i++, offset += 2) {
-                            let s = Math.max(-1, Math.min(1, input[i]))
-                            output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7fff, true)
-                        }
-                    }
-
-                    writeString(view, 0, 'RIFF') // RIFFヘッダ
-                    view.setUint32(4, 32 + samples.length * 2, true) // これ以降のファイルサイズ
-                    writeString(view, 8, 'WAVE') // WAVEヘッダ
-                    writeString(view, 12, 'fmt ') // fmtチャンク
-                    view.setUint32(16, 16, true) // fmtチャンクのバイト数
-                    view.setUint16(20, 1, true) // フォーマットID
-                    view.setUint16(22, 1, true) // チャンネル数
-                    view.setUint32(24, sampleRate, true) // サンプリングレート
-                    view.setUint32(28, sampleRate * 2, true) // データ速度
-                    view.setUint16(32, 2, true) // ブロックサイズ
-                    view.setUint16(34, 16, true) // サンプルあたりのビット数
-                    writeString(view, 36, 'data')// dataチャンク
-                    view.setUint32(40, samples.length * 2, true) // 波形データのバイト数
-                    floatTo16BitPCM(view, 44, samples) // 波形データ
-
-
-                    return view
-                }
-
-                let mergeBuffers = function (audioData) {
-                    let sampleLength = 0
-                    for (let i = 0; i < audioData.length; i++) {
-                        sampleLength += audioData[i].length
-                    }
-                    let samples = new Float32Array(sampleLength)
-                    let sampleIdx = 0
-                    for (let i = 0; i < audioData.length; i++) {
-                        for (let j = 0; j < audioData[i].length; j++) {
-                            samples[sampleIdx] = audioData[i][j]
-                            sampleIdx++
-                        }
-                    }
-
-                    return samples
-                }
-
-                let dataview = encodeWAV(mergeBuffers(this.audioData), this.audio_sample_rate)
-                let audioBlob = new Blob([dataview], {type: 'audio/wav'})
-                console.log(dataview)
-                console.log(audioBlob)
-                let reader = new FileReader()
-                reader.readAsDataURL(audioBlob)
-
-                reader.onload = function () {
-                    me.wav = reader.result.substr(22)
-                }
-
-                this.audioData = []
-                // let myURL = window.URL || window.webkitURL
-                // let url = myURL.createObjectURL(audioBlob)
-                // return url
-            },
             onAudioProcess(e) {
                 //オーディオ入力
                 let input = e.inputBuffer.getChannelData(0)
@@ -242,6 +265,8 @@
                 return newData
             },
             recStart() {
+                //ボタン連打防止用
+                this.isPush = true
                 //カメラマイクをONにし、録音を始める
                 this.video = this.$refs.video
                 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
@@ -253,23 +278,28 @@
                         this.localStream = stream
                         this.audioContext = new AudioContext()
                         this.audio_sample_rate_old = this.audioContext.sampleRate
+                        this.bufferSize = 1024
+                        console.log("bufferSizeは" + this.bufferSize)
                         this.scriptProcessor = this.audioContext.createScriptProcessor(this.bufferSize, 1, 1)
                         let mediastreamsource = this.audioContext.createMediaStreamSource(stream)
                         mediastreamsource.connect(this.scriptProcessor)
-                        this.scriptProcessor.onaudioprocess = this.onAudioProcess
-                        // １秒間隔で画像を切り取る
-                        this.timer = setInterval(this.capture, 4900)
+
+                        // this.scriptProcessor.onaudioprocess = this.onAudioProcess
+
+                        // 3秒間隔で画像を切り取る
+                        this.timer = setInterval(this.capture, 3000)
                         this.scriptProcessor.connect(this.audioContext.destination)
                         console.log('record start?')
 
-                        //１０秒後に録音を停止する
-                        setTimeout(this.recStop, 10000)
+                        //１1秒後に録音を停止する
+                        setTimeout(this.recStop, 11000)
                     })
                 } else {
                     console.log("getUserMedia not support")
                 }
             },
             recStop() {
+                this.isPush = false
                 //画像を切り取る処理を停止させる
                 clearInterval(this.timer)
                 console.log('saved wav')
